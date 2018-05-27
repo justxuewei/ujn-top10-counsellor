750dac1074c8246502743b0b27d159152d0dad8c
MrN1u <a@niuxuewei.com>
2018-05-27 17:39:00 +0800

    增加完成人数统计

--- application/statistics/controller/Index.php
+++ application/statistics/controller/Index.php
@@ -85,7 +85,7 @@ class Index extends SJController {
      */
     public function completion() {
         $this->jSuccess([
-            'count' => Db::table('invitation_code')->where('is_confirm', 1)->count()
+            'count' => Db::name('invitation_code')->where('is_confirm', 1)->count()
         ]);
     }
 
